96fa2949caa3d7e53abae4f1e49746c0e1629d2d
TerzinGO <terzingo@gmail.com>
2025-11-02 11:39:21 +0000

    anasayfadaki terzingo noktasını bulun yazan alan header kısmıdaki bazı y

--- src/components/find-tailor-form.tsx
+++ src/components/find-tailor-form.tsx
@@ -64,7 +64,7 @@ export function FindTailorForm() {
   return (
     <section id="find-tailor" className="w-full py-16 md:py-24">
       <div className="container mx-auto px-4 md:px-6">
-        <Card className="max-w-4xl mx-auto shadow-2xl -mt-48 relative z-20">
+        <Card className="max-w-4xl mx-auto shadow-2xl -mt-32 relative z-20">
           <CardHeader>
             <CardTitle className="text-2xl md:text-3xl font-bold font-headline text-center">
               Terzin<span className="text-primary">Go</span> Noktasını Bulun
@@ -98,7 +98,7 @@ export function FindTailorForm() {
                 <label className="font-semibold text-sm">İlçe</label>
                 <Select
                   disabled={!selectedCity}
-                  onValueChange={(value: any) => {
+                  onValuechange={(value: any) => {
                     setSelectedDistrict(value);
                     setSelectedNeighborhood(null);
                     setShowResults(false);
