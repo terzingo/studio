9206779671c94abd903651937fbb5ae973a82787
TerzinGO <terzingo@gmail.com>
2025-11-03 07:52:32 +0000

    şuan dosyalarımız githuba commit ediliyor. ve ordan da vercel ortamında

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,6 +27,7 @@ export default function RootLayout({
           href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap"
           rel="stylesheet"
         />
+        <link rel="icon" href="/favicon.ico" sizes="any" />
       </head>
       <body
         className={cn(
